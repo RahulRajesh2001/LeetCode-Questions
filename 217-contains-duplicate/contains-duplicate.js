6fc3e54e0342652a83290a233ba36a17502572ad
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-01-16 19:44:47 +0530

    Time: 87 ms (42.89%) | Memory: 55.3 MB (26.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set=new Set(nums);
+    if(set.size == nums.length){
+        return false
+    }else{
+        return true
+    }
+}
