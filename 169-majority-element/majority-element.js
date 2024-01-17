798e3092e6b9c5a33ee74caea7169c354d914a27
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-01-17 11:08:46 +0530

    Time: 65 ms (34.76%) | Memory: 43.6 MB (69.99%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums.sort((a,b) => a - b);
-    return nums[Math.floor(nums.length/2)];
-}; 
+ const setMap=new Map()
+ const n = Math.ceil(nums.length / 2);
+
+ for(let i=0;i<nums.length;i++){
+    let number=nums[i]
+    if(!setMap.has(number)){
+        setMap.set(number,1)
+    }else{
+        setMap.set(number,setMap.get(number)+1)
+    }
+ }
+
+setMap.forEach((occurence,number)=>{
+if(occurence>=n){
+    m=number;
+}
+
+})
+return m
+
+};
