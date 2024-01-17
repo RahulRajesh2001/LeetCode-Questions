d3575f62db3136fb5de2858d9f349d5d8525e23b
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-01-17 14:49:08 +0530

    Time: 55 ms (55.89%) | Memory: 41.9 MB (95.45%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const setMap=new Map()
+    for(i=0;i<arr.length;i++){
+        let number=arr[i];
+        if(!setMap.has(number)){
+            setMap.set(number,1)
+        }else{
+            setMap.set(number,setMap.get(number)+1)
+        }
+    }
+    const set=new Set()
+    setMap.forEach((occurence,number)=>{
+        set.add(occurence)
+    })
+
+if(setMap.size == set.size){
+    return true
+}else{
+    return false
+}
+
+};
