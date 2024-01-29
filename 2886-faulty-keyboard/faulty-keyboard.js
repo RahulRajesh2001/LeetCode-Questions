913c737d32bf469a841480036f6d2d3a0a6d075b
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-01-29 23:42:25 +0530

    Time: 88 ms (95.68%) | Memory: 54.6 MB (22.77%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.js
+++ 2886-faulty-keyboard/faulty-keyboard.js
@@ -4,18 +4,13 @@
  */
 var finalString = function(s) {
     let a=[]
-    let m=[]
     for(i=0;i<s.length;i++){
         a.push(s[i])
         if(s[i]=='i'){
                 a.reverse()
         }
     }
-for(let i=0;i<a.length;i++){
-    if(a[i]!="i"){
-        m.push(a[i])
-    }
-}
-return m.join('')
+
+ return a.join('').replaceAll('i','')
     
 }
