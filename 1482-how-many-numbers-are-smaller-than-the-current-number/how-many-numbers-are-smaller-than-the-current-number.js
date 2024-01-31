c938cb225ab203949fab36bcaac536afb4b4f2ee
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-01-31 21:06:43 +0530

    Time: 64 ms (81.16%) | Memory: 51.1 MB (17.29%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -4,9 +4,9 @@
  */
 var smallerNumbersThanCurrent = function(nums) {
     let n=[]
-    for(i=0;i<nums.length;i++){
+    for(let i=0;i<nums.length;i++){
         let c=0;
-        for(j=0;j<nums.length;j++){
+        for(let j=0;j<nums.length;j++){
             if(nums[i]>nums[j]){
                 if(i!=j){
                     c++;
