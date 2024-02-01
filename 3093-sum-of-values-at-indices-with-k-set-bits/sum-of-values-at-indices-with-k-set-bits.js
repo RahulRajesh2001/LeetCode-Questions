8dec55ebbda06d0607f7a8a3530b0906ebf45d5f
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-02-01 22:35:27 +0530

    Time: 78 ms (34.45%) | Memory: 51.6 MB (14.32%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.js
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.js
@@ -5,10 +5,10 @@
  */
 var sumIndicesWithKSetBits = function(nums, k) {
     let s=0;
-    for(i=0;i<nums.length;i++){
+    for(let i=0;i<nums.length;i++){
         let v=i.toString(2).padStart(3, '0')
         let c=0;
-        for(j=0;j<v.length;j++){
+        for(let j=0;j<v.length;j++){
             if(v[j]==1){
                 c++
             }
