c7568f2dac9ff5578548d40d6e7720ebde0390d3
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-02-09 15:55:33 +0530

    Time: 92 ms (39.58%) | Memory: 48.8 MB (34.18%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,8 +5,8 @@
  */
 
 var twoSum = function(nums, target) {
-    for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
             if(target==nums[i]+nums[j]){
                 return [i,j]
             }
