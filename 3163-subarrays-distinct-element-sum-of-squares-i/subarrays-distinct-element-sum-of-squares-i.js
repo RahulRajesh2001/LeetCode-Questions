6052c0d6a45ca570bfbf342dc54a37b3f3326675
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-02-12 23:37:46 +0530

    Time: 65 ms (86.75%) | Memory: 54.4 MB (44.87%) - LeetSync

--- 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.js
+++ 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.js
@@ -4,7 +4,7 @@
  */
 var sumCounts = function(nums) {
   let t=0;
-  for(i=0;i<nums.length;i++){
+  for(let i=0;i<nums.length;i++){
     let a=new Set()
     for(let j=i;j<nums.length;j++){
       a.add(nums[j]);
