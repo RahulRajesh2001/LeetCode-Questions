a429dd58a2e40e2ddd58a1dd2202f8d0adeb96c2
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-02-18 23:21:33 +0530

    Time: 79 ms (80.08%) | Memory: 52.6 MB (24.30%) - LeetSync

---
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+   nums.sort((a,b)=>{
+       return b-a
+       }) 
+       
+   for(let i=0;i<nums.length-2;i++){
+        if(nums[i+1]+nums[i+2]>nums[i]){
+       return nums[i+1]+nums[i+2]+nums[i]
+       } 
+}return 0}
