8f1af097d4c3fca25e9e5ff6ac00ce3ae791512b
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-02-25 19:26:45 +0530

    Time: 52 ms (86.23%) | Memory: 50.6 MB (32.02%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.js
@@ -0,0 +1,17 @@
+
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let arr = [];
+    let result = "";
+    for(let i=0; i<indices.length; i++) {
+        arr[indices[i]] = s.charAt(i);
+    }
+    for(let i=0; i<arr.length; i++) {
+        result = result + arr[i];
+    }
+    return result;
+};
