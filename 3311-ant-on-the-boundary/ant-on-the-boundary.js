93f67f99997ce072a0fb1ddc66b6f344a5cc980c
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-03-02 19:15:51 +0530

    Time: 53 ms (83.61%) | Memory: 49.2 MB (94.14%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.js
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.js
@@ -6,12 +6,8 @@ var returnToBoundaryCount = function(nums) {
     let c=0;
     let l=0
     for(let i=0;i<nums.length;i++){
-       
-    c=c+nums[i]
-        
-        if(c==0){
-            l=l+1;
-        }
+         c=c+nums[i]
+        if(c==0)l=l+1;
     }
     return l
 }
