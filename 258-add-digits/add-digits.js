5b017a11efa568e4e17baea6c54aedcf73802be5
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-03-20 22:03:25 +0530

    Time: 75 ms (66.07%) | Memory: 53.4 MB (49.40%) - LeetSync

--- 258-add-digits/add-digits.js
+++ 258-add-digits/add-digits.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var addDigits = function (num) {
-    if (num === 0) {
+    if (num == 0) {
         return 0
     }
-    if (num < 10) {
-        return num
-    }
+
     return num % 9 === 0 ? 9 : num % 9;
 }
