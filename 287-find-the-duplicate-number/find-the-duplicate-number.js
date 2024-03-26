3a5cf81b571724e688a569b27b403872dc24c3b3
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-03-26 08:33:04 +0530

    Time: 101 ms (31.57%) | Memory: 84.5 MB (5.00%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -2,21 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
+var findDuplicate = function (nums) {
     const map = new Map();
-    for (let i = 0; i < nums.length; i++){
-        if(map.has(nums[i])) {
-            map.set(nums[i], map.get(nums[i]) + 1);
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i],map.get(nums[i])+1)
         }else{
-            map.set(nums[i],1);
+            map.set(nums[i],1)
         }
     }
-    var duplicate=0
+    let duplicate=0
     map.forEach((occurence,num)=>{
         if(occurence>1){
-          duplicate=num
+            duplicate=num
         }
     })
     return duplicate
-    
 }
