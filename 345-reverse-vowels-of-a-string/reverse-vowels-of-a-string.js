a7ecc13e42e73d870983c18dfbdfd0f95bed6067
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-06 15:20:49 +0530

    Time: 211 ms (7.56%) | Memory: 61.6 MB (5.15%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    let word = s.split('')
+    console.log("hehe",word)
+    let array = []
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u'|| word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U') {
+            array.push(word[i])
+        }
+    }
+    let reverced = array.reverse()
+console.log("hehe",reverced)
+    let c = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' || word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U') {
+            console.log("executed")
+            word[i] = reverced[c]
+            c++
+        }
+    }
+
+
+    return word.join('').toString()
+};
