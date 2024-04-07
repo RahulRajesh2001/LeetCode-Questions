7ddf82fa21ce1dff42f82eb9789914b08046bed1
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-07 09:58:55 +0530

    Time: 54 ms (88.25%) | Memory: 50.4 MB (96.81%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -3,16 +3,16 @@
  * @param {number} target
  * @return {number}
  */
-var countPairs = function(nums, target) {
-    let c=0
-    for(let i=0;i<nums.length;i++){
-       for(let j=0;j<nums.length;j++){
-            if(i<j){
-                if(nums[i]+nums[j]<target){
+var countPairs = function (nums, target) {
+    let c = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i < j) {
+                if (nums[i] + nums[j] < target) {
                     c++
                 }
             }
-       } 
+        }
     }
     return c
 }
