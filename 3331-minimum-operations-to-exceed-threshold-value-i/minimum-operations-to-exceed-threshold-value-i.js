19644ff5a9e519b29e018501b277c819325a2b63
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-08 19:59:24 +0530

    Time: 56 ms (81.84%) | Memory: 51.1 MB (64.59%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let arr=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>=k){
+            arr.push(nums[i])
+        }
+    }
+    return nums.length-arr.length
+};
