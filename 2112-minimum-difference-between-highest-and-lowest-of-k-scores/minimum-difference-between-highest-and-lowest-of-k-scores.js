b9a721099d66f0ef86a98a38932e726a68207eb9
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-08 10:49:22 +0530

    Time: 65 ms (55.81%) | Memory: 51 MB (83.33%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -8,11 +8,14 @@ var minimumDifference = function(nums, k) {
         return 0
     }
     nums.sort((a,b)=>a-b)
-    let min=nums[0]
-    let max=nums[k-1]
-    let diff=max-min
-    for(let i=k;i<nums.length;i++){
-        diff=Math.min(diff,nums[i]-nums[i-k+1])
+    let left=0;
+    let right=k-1;
+    let result=Infinity;
+
+    while(right<nums.length){
+        result=Math.min(nums[right]-nums[left],result)
+        right++;
+        left++
     }
-    return diff
+    return result
 };
