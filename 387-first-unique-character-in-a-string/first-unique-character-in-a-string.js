993e0d79253ba96e8159c99b6215a056a102ad10
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-12 23:44:59 +0530

    Time: 106 ms (20.74%) | Memory: 52.8 MB (83.89%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -2,30 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {
-    let map=new Map()
+var firstUniqChar = function (s) {
+    let map = new Map()
 
-    for(let i=0;i<s.length;i++){
-        if(!map.has(s[i])){
-            map.set(s[i],1)
-        }else{
-            map.set(s[i],map.get(s[i])+1)
+    for (let i = 0; i < s.length; i++) {
+        if (!map.has(s[i])) {
+            map.set(s[i], 1)
+        } else {
+            map.set(s[i], map.get(s[i]) + 1)
         }
     }
-    let array=[]
-    map.forEach((value,key)=>{
-        if(value==1){
-            for(let i=0;i<s.length;i++){
-               if(key==s[i]){
-                array.push(i)
-               }
+    let array = []
+    map.forEach((value, key) => {
+        if (value == 1) {
+            for (let i = 0; i < s.length; i++) {
+                if (key == s[i]) {
+                    array.push(i)
+                }
             }
         }
     })
-    if(array.length==0){
-return -1
-    }else{
+    if (array.length == 0) {
+        return -1
+    } else {
         return array[0]
     }
-    
+
 };
