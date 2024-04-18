64a9d00c0ce7c0e996050b99a7b559246716efba
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-18 12:52:36 +0530

    Time: 57 ms (53.90%) | Memory: 51.5 MB (24.54%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.js
+++ 414-third-maximum-number/third-maximum-number.js
@@ -2,18 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function(nums) {
-    nums.sort((a,b)=>a-b)
-    const set=new Set(nums)
-    const arr=Array.from(set)
+var thirdMax = function (nums) {
+    nums.sort((a, b) => a - b)
+    const set = new Set(nums)
+    const arr = Array.from(set)
     arr.reverse()
-    
-    if(arr.length==1){
-       return arr[0] 
-    }else if(arr.length==2){
-        return arr[arr.length-2]
-    }else{
+
+    if (arr.length == 1) {
+        return arr[0]
+    } else if (arr.length == 2) {
+        return arr[arr.length - 2]
+    } else {
         return arr[2]
     }
-    
 };
