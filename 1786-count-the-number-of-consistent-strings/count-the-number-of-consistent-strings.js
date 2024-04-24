4eb8a5f6b77d7bae9ecee50142ab399824240630
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-04-24 16:27:26 +0530

    Time: 121 ms (9.68%) | Memory: 58.9 MB (53.76%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.js
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var countConsistentStrings = function (allowed, words) {
-    let k=0
-    for(let i=0;i<words.length;i++){
-        let c=0;
-        for(let j=0;j<allowed.length;j++){
-            let ch=words[i]
-            for(let k=0;k<words[i].length;k++){
-                if(ch[k]==allowed[j]){
-                    c=c+1
+    let k = 0
+    for (let i = 0; i < words.length; i++) {
+        let c = 0;
+        for (let j = 0; j < allowed.length; j++) {
+            let ch = words[i]
+            for (let k = 0; k < words[i].length; k++) {
+                if (ch[k] == allowed[j]) {
+                    c = c + 1
                 }
             }
         }
-       if(c>=words[i].length){
-        k=k+1
-       }
+        if (c >= words[i].length) {
+            k = k + 1
+        }
     }
     return k
 };
