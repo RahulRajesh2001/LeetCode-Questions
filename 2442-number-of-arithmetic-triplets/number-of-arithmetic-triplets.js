df3ab41016468ef769ded204bce425e4a70e0d4b
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-05-05 07:47:16 +0530

    Time: 50 ms (86.06%) | Memory: 49.1 MB (63.81%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.js
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.js
@@ -3,21 +3,11 @@
  * @param {number} diff
  * @return {number}
  */
-var arithmeticTriplets = function(nums, diff) {
-    let c=0
-    for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length;j++){
-            for(let k=0;k<nums.length;k++){
-                if(i != j && j!=k && k != i ){
-                    if(i<j<k){
-                        if(nums[j]-nums[i]==diff){
-                            if(nums[k]-nums[j]==diff){
-                                c=c+1;
-                            }
-                        }
-                    }
-                }
-            }
+var arithmeticTriplets = function (nums, diff) {
+    let c = 0
+    for (let num of nums) {
+        if (nums.includes(num + diff) && nums.includes(num + diff * 2)) {
+            c++
         }
     }
     return c
