2e935b32f69330a6497374139254f140bd131e08
Rahul Rajesh <105097737+RahulRajesh2001@users.noreply.github.com>
2024-05-20 15:46:19 +0530

    Time: 56 ms (35.52%) | Memory: 48.7 MB (58.97%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-  let c=0;
-  for(let i=0;i<nums.length;i++){
-      for(let j=i+1;j<nums.length;j++){
-          if(i!=j){
-              if(nums[i]==nums[j]){
-              c++;
-          }
-          }
-      }
-  }
-  return c;
+    let c=0
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+                if(nums[i] == nums[j]){
+                    if(i<j){
+                        c++
+                    }
+                } 
+        }
+    }
+    return c
 };
